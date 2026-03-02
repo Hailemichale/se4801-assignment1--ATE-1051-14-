df678b62f7c78719ef465efd9de5275384dd953e
Hailemichale <hailemichallijalem@gmail.com>
2026-03-02 14:42:50 +0300

    main entry file for spring application

--- src/main/java/com/course/enterpriseapplicationdevelopment/EnterpriseApplicationDevelopmentApplication.java
+++ src/main/java/com/course/enterpriseapplicationdevelopment/EnterpriseApplicationDevelopmentApplication.java
@@ -9,5 +9,4 @@ public class EnterpriseApplicationDevelopmentApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseApplicationDevelopmentApplication.class, args);
     }
-
 }
