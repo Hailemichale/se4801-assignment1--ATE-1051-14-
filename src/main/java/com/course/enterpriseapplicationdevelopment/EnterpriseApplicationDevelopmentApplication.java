f33a96b694fb14c75acc3688cfbd5b35ae75cd7a
Hailemichale <hailemichallijalem@gmail.com>
2026-03-02 14:40:48 +0300

    add source folder

---
+++ src/main/java/com/course/enterpriseapplicationdevelopment/EnterpriseApplicationDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.course.enterpriseapplicationdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseApplicationDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseApplicationDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/enterpriseapplicationdevelopment/EnterpriseApplicationDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.enterpriseapplicationdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
